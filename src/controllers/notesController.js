0bdb8fdffc391b9b20b194e7ed42cbeea79fd5f4
Dmitro <dimasik93.ua@gmail.com>
2026-05-07 14:48:29 +0300

    hw2

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -12,7 +12,7 @@ export const getNoteById = async (req, res) => {
   if (!note) {
    throw createHttpError(404, "Note not found");
   }
-  res.status(200).json({ note });
+  res.status(200).json(note);
 
 };
 
