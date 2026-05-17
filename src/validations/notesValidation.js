30331723d32a28f11a4ea565eef40b65e75f212f
Dmitro <dimasik93.ua@gmail.com>
2026-05-17 15:23:03 +0300

    hw3

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -6,7 +6,7 @@ export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
-    tag: Joi.string().valid(...TAGS),
+    tag: Joi.string().valid(...TAGS).optional(),
   search: Joi.string().allow('').optional(),
   }),
 };
@@ -25,8 +25,8 @@ export const noteIdSchema = {
 export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).required(),
-    content: Joi.string().allow(''),
-    tag: Joi.string().valid(...TAGS),
+    content: Joi.string().allow('').optional(),
+    tag: Joi.string().valid(...TAGS).optional(),
   }),
 };
 
