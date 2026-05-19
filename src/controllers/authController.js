ddd2e0912de34f3b38441eb1063c3c53d8f7643c
Dmitro <dimasik93.ua@gmail.com>
2026-05-19 10:50:05 +0300

    hw4 p1

---
+++ src/controllers/authController.js
@@ -0,0 +1,53 @@
+import createHttpError from 'http-errors';
+
+import { Session } from '../models/session.js';
+import { createSession } from '../services/auth.js';
+import { setSessionCookies } from '../utils/setSessionCookies.js';
+
+export const refreshUserSession = async (req, res) => {
+  const { sessionId, refreshToken } = req.cookies;
+
+  if (!sessionId || !refreshToken) {
+    throw createHttpError(401, 'Session not found');
+  }
+
+  const session = await Session.findOne({
+    _id: sessionId,
+    refreshToken,
+  });
+
+  if (!session) {
+    throw createHttpError(401, 'Session not found');
+  }
+
+  const isRefreshTokenExpired =
+    new Date() > new Date(session.refreshTokenValidUntil);
+
+  if (isRefreshTokenExpired) {
+    throw createHttpError(401, 'Session token expired');
+  }
+
+  await Session.deleteOne({ _id: session._id });
+
+  const newSession = await createSession(session.userId);
+
+  setSessionCookies(res, newSession);
+
+  res.status(200).json({
+    message: 'Session refreshed',
+  });
+};
+
+export const logoutUser = async (req, res) => {
+  const { sessionId } = req.cookies;
+
+  if (sessionId) {
+    await Session.deleteOne({ _id: sessionId });
+  }
+
+  res.clearCookie('sessionId');
+  res.clearCookie('accessToken');
+  res.clearCookie('refreshToken');
+
+  res.status(204).send();
+};
--- src/server.js
+++ src/server.js
@@ -1,23 +1,27 @@
-  import express from 'express';
+import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import { errors } from "celebrate";
+import cookieParser from "cookie-parser";
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 const app = express();
 
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 
 
 const PORT = process.env.PORT ?? 3000;
 
 app.use(notesRoutes);
+app.use(authRoutes);
 app.use(notFoundHandler);
 app.use(errors());
 app.use(errorHandler);
