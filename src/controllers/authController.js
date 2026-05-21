2252197f9d7f75b59475cf7ed9dfd0c96828168b
Dmitro <dimasik93.ua@gmail.com>
2026-05-21 10:50:12 +0300

    hw4

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -77,6 +77,12 @@ export const refreshUserSession = async (req, res) => {
     new Date() > new Date(session.refreshTokenValidUntil);
 
   if (isRefreshTokenExpired) {
+    await Session.deleteOne({ _id: session._id });
+
+    res.clearCookie('sessionId');
+    res.clearCookie('accessToken');
+    res.clearCookie('refreshToken');
+
     throw createHttpError(401, 'Session token expired');
   }
 
